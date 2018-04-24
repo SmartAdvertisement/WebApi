08fbfaae3e29d7b7ce775c563ca897a1c9748700
Berat Nakip <g141210084@sakarya.edu.tr>
2018-04-24 12:23:27 +0300

    File upload tamamlandı

--- src/main/java/com/sas/webapi/Model/Users.java
+++ src/main/java/com/sas/webapi/Model/Users.java
@@ -14,7 +14,7 @@ import java.util.Set;
 public class Users implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="user_id")
+    @Column(name="id")
     private Integer id;
 
     @JsonProperty("name")
